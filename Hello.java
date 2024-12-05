f39cdc9d49c2744a98362b01f4932e873bca03c5
JYJ <joy6924@hansung.ac.kr>
2024-12-05 10:27:35 +0000

    G

--- Hello.java
+++ Hello.java
@@ -1,3 +1,4 @@
 public class Hello { public static void main(String[] args) { System.out.println("Hello, A!"); } }
 System.out.println("Hello, B!");
 System.out.println("Hello, C!");
+System.out.println("Hello,G!");
